938f8e1b1803869d547ad09f70d85ee726403295
jang young kyu <qto1@naver.com>
2023-12-27 16:20:03 +0900

    "first commit"

---
+++ src/main/java/hello/servlet/basic/HelloData.java
@@ -0,0 +1,12 @@
+package hello.servlet.basic;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+
+@Getter
+@Setter
+public class HelloData {
+    private String username;
+    private int age;
+}
